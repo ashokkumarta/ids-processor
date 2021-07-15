4b71026cb78c23428f1b173e1faf0eae704e371e
Ashokkumar T.A <ashokkumar.ta@gmail.com>
2021-07-15 10:50:28 +0530

    first commit

---
+++ c.bat
@@ -0,0 +1,6 @@
+call b
+del D:\Ashok\Work\IDS\git\ids-trusted-connector\ids-connector\build\libs\libraryJars\ids-processor-0.1-SNAPSHOT.jar
+copy D:\Ashok\Work\IDS\wa\ids-processor\build\libs\ids-processor-0.1-SNAPSHOT.jar D:\Ashok\Work\IDS\git\ids-trusted-connector\ids-connector\build\libs\libraryJars\ids-processor-0.1-SNAPSHOT.jar
+cd D:\Ashok\Work\IDS\git\ids-trusted-connector
+call d
+cd D:\Ashok\Work\IDS\wa\ids-processor
